b0c4ba6f00221750192e7aa3a435b2af45b92a51
erkyz <erkyz@aim.com>
2017-10-26 02:59:41 -0400

    hrnn

---
+++ torch_models/train_hrnn.py
@@ -0,0 +1,216 @@
+
+import torch
+import argparse
+import time
+import math
+import torch.nn as nn
+from torch.autograd import Variable
+import sys, os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import torchrnnlm
+import data
+import util
+
+parser = argparse.ArgumentParser(description='PyTorch MIDI RNN/LSTM Language Model')
+
+parser.add_argument('--data', type=str, default='../music_data/Nottingham/',
+                    help='location of the data corpus')
+parser.add_argument('--model', type=str, default='LSTM',
+                    help='type of recurrent net (RNN_TANH, RNN_RELU, LSTM, GRU)')
+parser.add_argument('--emsize', type=int, default=200,
+                    help='size of word embeddings')
+parser.add_argument('--nhid', type=int, default=200,
+                    help='number of hidden units per layer')
+parser.add_argument('--nlayers', type=int, default=2,
+                    help='number of layers')
+parser.add_argument('--lr', type=float, default=10,
+                    help='initial learning rate')
+parser.add_argument('--clip', type=float, default=0.25,
+                    help='gradient clipping')
+parser.add_argument('--epochs', type=int, default=40,
+                    help='upper epoch limit')
+parser.add_argument('--bptt', type=int, default=35,
+                    help='sequence length') # Not used right now.
+parser.add_argument('--dropout', type=float, default=0.2,
+                    help='dropout applied to layers (0 = no dropout)')
+parser.add_argument('--tied', action='store_true',
+                    help='tie the word embedding and softmax weights')
+parser.add_argument('--attention', action='store_true',
+                    help='batch size')
+parser.add_argument('--seed', type=int, default=1111,
+                    help='random seed')
+parser.add_argument('--cuda', action='store_true',
+                    help='use CUDA')
+parser.add_argument('--log-interval', type=int, default=200, metavar='N',
+                    help='report interval')
+parser.add_argument('--save', type=str,  default='../tmp/model.pt',
+                    help='path to save the final model')
+args = parser.parse_args()
+
+print args
+
+# Set the random seed manually for reproducibility.
+torch.manual_seed(args.seed)
+if torch.cuda.is_available():
+    if not args.cuda:
+        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+    else:
+        torch.cuda.manual_seed(args.seed)
+
+###############################################################################
+# Load data
+###############################################################################
+
+sv = util.SimpleVocab.load_from_corpus(args.data, "../tmp/nott_sv.p")
+corpus = data.Corpus(args.data, sv)
+
+args.batch_size = 1
+
+def batchify(data, bsz):
+    # Work out how cleanly we can divide the dataset into bsz parts.
+    nbatch = data.size(0) // bsz
+    # Trim off any extra elements that wouldn't cleanly fit (remainders).
+    data = data.narrow(0, 0, nbatch * bsz)
+    # Evenly divide the data across the bsz batches.
+    data = data.view(bsz, -1).t().contiguous()
+    if args.cuda:
+        data = data.cuda()
+    return data
+
+eval_batch_size = 10
+
+train_data = batchify(corpus.train, args.batch_size)
+val_data = batchify(corpus.valid, eval_batch_size)
+test_data = batchify(corpus.test, eval_batch_size)
+train_masks = batchify(corpus.train_masks, args.batch_size)
+val_masks = batchify(corpus.valid_masks, eval_batch_size)
+test_masks = batchify(corpus.test_masks, eval_batch_size)
+
+###############################################################################
+# Build the model
+###############################################################################
+
+LOW_OUT_DIM = 30
+
+
+ntokens = sv.size
+
+model = torchrnnlm.RNNModel(args.model, low_out_dim, args.emsize, args.nhid, args.nlayers, args.dropout, args.attention)
+top_model = torchrnnlm.RNNModel(args.model, ntokens, low_out_dim, args.nhid, args.nlayers, args.dropout, args.attention)
+if args.cuda:
+    model.cuda()
+    top_model.cuda()
+
+criterion = nn.CrossEntropyLoss()
+
+###############################################################################
+# Training code
+###############################################################################
+
+def repackage_hidden(h):
+    """Wraps hidden states in new Variables, to detach them from their history."""
+    if type(h) == Variable:
+        return Variable(h.data)
+    else:
+        return tuple(repackage_hidden(v) for v in h)
+
+
+def get_batch(source, i, seq_len, evaluation=False):
+    this_seq_len = min(seq_len, len(source) - 1 - i)
+    data = Variable(source[i:i+this_seq_len], volatile=evaluation)
+    target = Variable(source[i+1:i+1+this_seq_len].view(-1))
+    return data, target
+
+
+def evaluate(data_source, data_masks, bptt):
+    # Turn on evaluation mode which disables dropout.
+    model.eval()
+    total_loss = 0
+    ntokens = len(corpus.dictionary)
+    hidden_low, hidden_high = model.init_hidden(eval_batch_size)
+    for i in range(0, data_source.size(0) - 1, bptt):
+        data, targets = get_batch(data_source, i, bptt, evaluation=True)
+        masks, _ = get_batch(data_masks, i, bptt, evaluation=True)
+        output, hidden = model(data, hidden)
+        total_loss += len(data) * eval_batch_size * bptt * criterion(output.view(-1, ntokens), targets) / masks.view(-1).sum(-1)
+        hidden = repackage_hidden(hidden)
+    return total_loss.data[0] / len(data_source)
+
+
+def train():
+    # Turn on training mode which enables dropout.
+    model.train()
+    total_loss = 0
+    start_time = time.time()
+    ntokens = len(corpus.dictionary)
+    hidden_low, hidden_high = model.init_hidden(args.batch_size)
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, corpus.train_maxlen)):
+        data, targets = get_batch(train_data, i, corpus.train_maxlen)
+        masks, _ = get_batch(train_masks, i, corpus.train_maxlen)
+        # Starting each batch, we detach the hidden state from how it was previously produced.
+        # If we didn't, the model would try backpropagating all the way to start of the dataset.
+        hidden_low = repackage_hidden(hidden_low)
+        hidden_high = repackage_hidden(hidden_high)
+        model.zero_grad()
+        output, hidden = model(data, hidden_low, hidden_high)
+        loss = args.batch_size * corpus.train_maxlen * criterion(output.view(-1, ntokens), targets) / masks.view(-1).sum(-1)
+        loss.backward()
+
+        # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
+        torch.nn.utils.clip_grad_norm(model.parameters(), args.clip)
+        for p in model.parameters():
+            p.data.add_(-lr, p.grad.data)
+
+        total_loss += loss.data
+
+        if batch % args.log_interval == 0 and batch > 0:
+            cur_loss = total_loss[0] / args.log_interval
+            elapsed = time.time() - start_time
+            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
+                    'loss {:5.2f} | ppl {:8.2f}'.format(
+                epoch, batch, len(train_data) // args.bptt, lr,
+                elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
+            total_loss = 0
+            start_time = time.time()
+
+# Loop over epochs.
+lr = args.lr
+best_val_loss = None
+
+# At any point you can hit Ctrl + C to break out of training early.
+try:
+    for epoch in range(1, args.epochs+1):
+        epoch_start_time = time.time()
+        train()
+        val_loss = evaluate(val_data, val_masks, corpus.valid_maxlen)
+        print('-' * 89)
+        print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
+                'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
+                                           val_loss, math.exp(val_loss)))
+        print('-' * 89)
+        # Save the model if the validation loss is the best we've seen so far.
+        if not best_val_loss or val_loss < best_val_loss:
+            with open(args.save, 'wb') as f:
+                torch.save(model, f)
+            best_val_loss = val_loss
+        else:
+            # Anneal the learning rate if no improvement has been seen in the validation dataset.
+            lr /= 4.0
+except KeyboardInterrupt:
+    print('-' * 89)
+    print('Exiting from training early')
+
+# Load the best saved model.
+with open(args.save, 'rb') as f:
+    model = torch.load(f)
+
+# Run on test data.
+test_loss = evaluate(test_data, test_masks, corpus.test_maxlen)
+print('=' * 89)
+print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
+    test_loss, math.exp(test_loss)))
+print('=' * 89)
+
+
