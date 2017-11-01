83cb3fe76744cdb7761d5df8f3d9af0cc4ace8db
erkyz <erkyz@aim.com>
2017-11-01 02:36:23 -0400

    remove no enc model

--- torch_models/torchrnnlm.py
+++ torch_models/torchrnnlm.py
@@ -108,49 +108,6 @@ class RNNCellModel(nn.Module):
             return Variable(weight.new(bsz, self.nhid).zero_())
 
 
-class RNNModelNoEnc(nn.Module):
-    """Container module with a recurrent module, and a decoder (no embedding)"""
-
-    def __init__(self, rnn_type, ntoken, nhid, nlayers, dropout=0.1, attention=False):
-        super(RNNModelNoEnc, self).__init__()
-        self.drop = nn.Dropout(dropout)
-        # Choose attention model
-        if rnn_type in ['LSTM', 'GRU']:
-            self.rnn = getattr(nn, rnn_type)(ntoken, nhid, nlayers, dropout=dropout)
-        else:
-            try:
-                nonlinearity = {'RNN_TANH': 'tanh', 'RNN_RELU': 'relu'}[rnn_type]
-            except KeyError:
-                raise ValueError( """An invalid option for `--model` was supplied,
-                                 options are ['LSTM', 'GRU', 'RNN_TANH' or 'RNN_RELU']""")
-            self.rnn = nn.RNN(ntoken, nhid, nlayers, nonlinearity=nonlinearity, dropout=dropout)
-        self.decoder = nn.Linear(nhid, ntoken)
-
-        self.init_weights()
-
-        self.rnn_type = rnn_type
-        self.nhid = nhid
-        self.nlayers = nlayers
-
-    def init_weights(self):
-        initrange = 0.1
-        self.decoder.bias.data.fill_(0)
-        self.decoder.weight.data.uniform_(-initrange, initrange)
-
-    def forward(self, input, hidden):
-        output, hidden = self.rnn(input, hidden)
-        output = self.drop(output)
-
-        decoded = self.decoder(output.view(output.size(0)*output.size(1), output.size(2)))
-        return decoded.view(output.size(0), output.size(1), decoded.size(1)), hidden
-
-    def init_hidden(self, bsz):
-        weight = next(self.parameters()).data
-        if self.rnn_type == 'LSTM':
-            return (Variable(weight.new(self.nlayers, bsz, self.nhid).zero_()),
-                    Variable(weight.new(self.nlayers, bsz, self.nhid).zero_()))
-        else:
-            return Variable(weight.new(self.nlayers, bsz, self.nhid).zero_())
 
 # TODO pass vocab
 MEASURE_TOKEN_IDX = 3
