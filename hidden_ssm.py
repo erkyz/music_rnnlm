080cb2d59a1ce13be4fc33737fef7d6af01e7531
erkyz <erkyz@aim.com>
2018-03-04 23:13:27 -0500

    local: hidden ssm

--- hidden_ssm.py
+++ hidden_ssm.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pickle
 
 ssm = pickle.load(open("test2.p", "rb"))
-plt.imshow(ssm, cmap='gray')
+f = np.vectorize(lambda x : x >= 0.95)
+plt.imshow(f(ssm), cmap='gray')
 plt.show()
 
